45966b07f1afce9175c3e004dc9f23076d07df48
brent stockton <brentstockton@brents-MacBook-Pro.local>
2021-10-19 01:55:38 -0700

    Added 2.4 AWS

---
+++ Assignment1/Gradle/JavaGradle/src/main/java/Fraction.java
@@ -0,0 +1,86 @@
+import java.io.*;
+/**
+ * Purpose: demonstrate simple Java Fraction class with command line,
+ * jdb debugging, and Ant build file.
+ *
+ * Ser321 Foundations of Distributed Applications
+ * see http://pooh.poly.asu.edu/Ser321
+ * @author Tim Lindquist Tim.Lindquist@asu.edu
+ *         Software Engineering, CIDSE, IAFSE, ASU Poly
+ * @version January 2020
+ */
+public class Fraction {
+
+   private int numerator, denominator;
+
+
+   public Fraction() {
+      numerator = denominator = 0;
+   }
+
+   public void print() {
+      System.out.print(numerator + " / " + denominator);
+   }
+
+   public void setNumerator(int n) {
+      numerator = n;
+   }
+
+   public void setDenominator(int d) {
+      denominator = d;
+   }
+
+   public int getDenominator() {
+      return denominator;
+   }
+
+   public int getNumerator() {
+      return numerator;
+   }
+
+   public static void main(String args[]) {
+      if (args.length == 2) {
+         int argX = 0;
+         int argY = 0;
+         try {
+            argX = Integer.parseInt(args[0]);
+            argY = Integer.parseInt(args[1]);
+            Fraction frac = new Fraction();
+            frac.setNumerator(argX);
+            frac.setDenominator(argY);
+            System.out.print("The fraction is: ");
+            frac.print();
+            System.out.println("");
+         } catch (Exception e) {
+            System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
+            System.exit(1);
+         }
+      } else {
+         System.out.println("Exactly 2 arguments should be provided.\n gradle run --args='1 2'");
+      }
+      /*try {
+         // create a new instance
+         // Fraction *frac = [[Fraction alloc] init];
+         Fraction frac = new Fraction();
+
+         Scanner s = new Scanner(System.in);
+         System.out.print("Enter : Numerator");
+
+         num = s.nextInt();
+         frac.setNumerator(num);
+         frac.setDenominator(3);
+
+         // print it
+         System.out.print("The fraction is: ");
+         frac.print();
+         System.out.println("");
+
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   } */
+
+   }
+}
+
+
